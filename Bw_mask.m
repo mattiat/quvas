ce85545dd86311e502c2a8cfcf58242ef6aec812
Mattia Tomasoni <tomasonimattia@gmail.com>
2018-05-04 09:58:16 +0200

    Added folder structure for I/O. Removed some duplicated code in Rc_mask.m

--- Bw_mask.m
+++ Bw_mask.m
@@ -1,47 +1,36 @@
-%%%% Changing to red color:
-img = imread('*.tif'); % Read image
-red = img(:,:,1); % Red channel
-a = zeros(size(img, 1), size(img, 2));
-just_red = cat(3, red, a, a);
-imshow(just_red,just_green,just_blue)
-figure, imshow(just_red), title('Red channel')
 
-% same possible with green or blue
-green = img(:,:,2); % Green channel
-just_green = cat(3, a, green, a);
-blue = img(:,:,3); % Blue channel
-just_blue = cat(3, a, a, blue);
+% set input and output locations
+input_dir = './input/DRIVE_database/';
+output_dir = './output/';
 
-
-
-
-
-%%%% Code Red Color + mask
-files = dir('*.tif');
-% loop over all tif files in the current folder
-results =fopen('results.csv', 'w' );
-%opens (or create if it did not exist) a result file in current folder
+files = dir(strcat(input_dir, '*.tif'));
+% loop over all tif files in the input folder
+results =fopen(strcat(output_dir, 'results.csv'), 'w' );
+%opens (or create if it did not exist) a result file in output folder
 for file = files'
     % load the image
-    img = imread(file.name);
-    filename = split(file.name,".");
+    img = imread(strcat(input_dir,file.name));
+    % filename = split(file.name,'.'); <- works for earlier MATLAB versions
+    filename = strsplit(file.name,'.');
     % splits the filename at the dot
     name = filename{1};
     % stores the file name whithout its extension as a string
-    MaskName = strcat(name,'_mask.gif');
+    MaskName = strcat(input_dir,name,'_mask.gif');
     %create the name of mask corresponding to current image
     mask = imread(MaskName);
+    
     % convert the image to red color
-    img = imread('01_test.tif');
     red = img(:,:,1);
     a = zeros(size(img, 1), size(img, 2));
     just_red = cat(3, red, a, a);
+    % store the converted image to disk for debugging
+    imwrite(just_red, strcat(output_dir,file.name(1:end-4),'_red.jpg'));
+    
     % convert the image to black and white
     grayscale = rgb2gray(just_red);
     imgBW=imbinarize(grayscale,'adaptive','sensitivity',0.63);
-    
     % store the converted image to disk for debugging
-    imwrite(imgBW, strcat(file.name(1:end-4),'_bw.jpg'));
+    imwrite(imgBW, strcat(output_dir,file.name(1:end-4),'_bw.jpg'));
     
     % counters for total number of white and black pixels in the image
     nWhite_total = 0; % set to 0 before we begin
@@ -84,5 +73,6 @@ for file = files'
     true_Black_total = nBlack_total-nBlack_mask;
     fprintf(results, '%s,%d,%d\n',name,true_Black_total, nWhite_total);
     %prints the file name and pixel counts to a new line in the csv file
+    
 end
 fclose(results);
