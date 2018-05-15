7fa027ef171b3bb04e9fdbc5ede2d9c611349e1a
Mattia Tomasoni <tomasonimattia@gmail.com>
2018-05-15 17:34:44 +0200

    feedback on last push

--- convert_to_green.m
+++ convert_to_green.m
@@ -7,8 +7,8 @@
 function img_green = convert_to_green(img, show_debug_imges)
 
 green_channel = img(:,:,2); % Green chanel
-b = zeros(size(img, 1), size(img, 2);
-img_green = cat(3, green_channel, a, a);
+b = zeros(size(img, 1), size(img, 2); % THERE IS A SYNTAX ERROR HERE
+img_green = cat(3, green_channel, a, a); % THERE IS A SEMANTIC ERROR HERE
 if show_debug_images
     figure, imshow(img_green), title('Green channel');
 end
