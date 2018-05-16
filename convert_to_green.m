c8c8dd976dec884bdc74ac12e68074a0f3defc0e
Loris Sonno <39093428+Billabooo@users.noreply.github.com>
2018-05-16 11:43:16 +0200

    Corrected the synthax and semantic errors

--- convert_to_green.m
+++ convert_to_green.m
@@ -6,10 +6,10 @@
 
 function img_green = convert_to_green(img, show_debug_imges)
 
-green_channel = img(:,:,2); % Green chanel
-b = zeros(size(img, 1), size(img, 2); % THERE IS A SYNTAX ERROR HERE
-img_green = cat(3, green_channel, a, a); % THERE IS A SEMANTIC ERROR HERE
-if show_debug_images
+green_channel = img(:,:,2); % Green channel
+b = zeros(size(img, 1), size(img, 2));
+img_green = cat(3, green_channel, b, b);
+if show_debug_imges
     figure, imshow(img_green), title('Green channel');
 end
 
